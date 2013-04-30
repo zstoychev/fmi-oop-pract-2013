d5a69e7ca04ba774e55f43b3bd638b4b6c9f20c1
Zdravko Stoychev <zstoychev@gmail.com>
2013-04-30 20:20:51 +0300

    Added the right solution for the MBR problem

--- exercises/03/solution/p4_mbr.cpp
+++ exercises/03/solution/p4_mbr.cpp
@@ -29,12 +29,35 @@ void printPartitionType(unsigned char partitionType) {
 		cout << "NTFS partition";
 		break;
 	default:
-		cout << "unknown partition";
+		cout << "Unknown partition";
 	}
 
 	cout << ")" << dec << endl;
 }
 
+int readInteger(istream& input) {
+	unsigned char chunks[4];
+
+	input.read((char*) chunks, sizeof(chunks));
+
+	return (chunks[3] << 24) + (chunks[2] << 16) + (chunks[1] << 8) + chunks[0];
+}
+
+/*
+ * The integers in the MBR file are stored in little endian format (read more at
+ * http://en.wikipedia.org/wiki/Endianness). Basically, the endianness specifies how
+ * the bytes in the integers are ordered in the memory (whether the most significant (Big Endian)
+ * or the least significant (Little Endian) byte is first).
+ *
+ * Here we demonstrate two ways for reading little endian integers:
+ *
+ * - The first one makes use of that the x86 architectures uses little endianness and directly
+ * reads the bytes in the memory. This solution is simple, but not multiplatform.
+ *
+ * - The second one (the function readInteger above of this comment) reads a chunk of 4 bytes
+ * and orders them properly using binary operations. This solution is platform-independent.
+ *
+ */
 void printPartitionInfo(istream& input) {
 	unsigned char activeFlag;
 	unsigned char partitionType;
@@ -43,10 +66,12 @@ void printPartitionInfo(istream& input) {
 
 	input.read((char*) &activeFlag, 1);
 	input.ignore(3);
-	input.read((char *) &partitionType, 1);
+	input.read((char*) &partitionType, 1);
 	input.ignore(3);
-	input.read((char *) &firstSector, sizeof(firstSector));
-	input.read((char *) &numberOfSectors, sizeof(numberOfSectors));
+
+	// Reading the two integers in two different ways
+	input.read((char*) &firstSector, sizeof(firstSector));
+	numberOfSectors = readInteger(input);
 
 	if (partitionType == 0x00) {
 		cout << "Empty partition entry\n";
@@ -72,11 +97,19 @@ void printPartitionsInfo(istream& input) {
 	}
 }
 
-int main() {
-	ifstream input("sdabeginning.img");
+
+int main(int argc, char* argv[]) {
+	const char* inputFileName = argc >= 2 ? argv[1] : "mbr.img";
+	ifstream input(inputFileName, ios::in | ios::binary);
+
+	if (!input) {
+		cout << "Can't open file " << inputFileName << endl;
+		return 1;
+	}
 
 	if (!checkSignature(input)) {
 		cout << "The file doesn't start with a MBR sector\n";
+		return 1;
 	}
 
 	printPartitionsInfo(input);
